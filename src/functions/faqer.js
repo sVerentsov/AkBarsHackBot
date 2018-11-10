a7950e3a880b8aa5df3d7ccc7ad6b9d969945d78
Sergey Verentsov <north1605@list.ru>
2018-11-11 01:30:57 +0300

    added empty hypotheses logic

--- src/functions/faqer.js
+++ src/functions/faqer.js
@@ -15,35 +15,40 @@ function faqer_query(client, message) {
             log(JSON.stringify(data));
             var hypotheses = _.filter(data.hypothesis, function(ans) {return ans.answer.indexOf("{{") !== -1; });
             hypotheses = _.sortBy(hypotheses, 'score');
-            var answer = hypotheses[hypotheses.length - 1].answer;
-            var id = hypotheses[hypotheses.length - 1].answerId;
-            var score = hypotheses[hypotheses.length - 1].score;
+            if(hypotheses.length == 0)
+            {
+                var answer = hypotheses[hypotheses.length - 1].answer;
+                var id = hypotheses[hypotheses.length - 1].answerId;
+                var score = hypotheses[hypotheses.length - 1].score;
 
-            if (score > +$global.constants.faqerThreshold) {
-                //cut link from answer
-                var words = answer.split(" ");
-                var link;
-                var button_exists = 0;
-                for (var i = 0; i < words.length; i++) {
-                    if (words[i].indexOf("http") > -1) { //check that word begins from http
-                        link = words[i];
-                        answer = answer.replace(words[i], "");
-                        button_exists = 1;
-                    }
-                }
-
-                //add link to button
-                if (button_exists == 1) {
-                    $reactions.buttons([{
-                        button: {
-                            text: "Ссылка",
-                            url: link,
-                            hide: false
+                if (score > +$global.constants.faqerThreshold) {
+                    //cut link from answer
+                    var words = answer.split(" ");
+                    var link;
+                    var button_exists = 0;
+                    for (var i = 0; i < words.length; i++) {
+                        if (words[i].indexOf("http") > -1) { //check that word begins from http
+                            link = words[i];
+                            answer = answer.replace(words[i], "");
+                            button_exists = 1;
                         }
-                    }]);
+                    }
+
+                    //add link to button
+                    if (button_exists == 1) {
+                        $reactions.buttons([{
+                            button: {
+                                text: "Ссылка",
+                                url: link,
+                                hide: false
+                            }
+                        }]);
+                    }
+                    $reactions.answer(answer);
+                    $reactions.transition("/");
+                } else {
+                    $reactions.answer("Извините, я не нашёл ответ!");    
                 }
-                $reactions.answer(answer);
-                $reactions.transition("/");
             } else {
                 $reactions.answer("Извините, я не нашёл ответ!");
             }
