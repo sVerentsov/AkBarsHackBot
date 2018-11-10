1081889733c150b73693cffdac79574ae3989ab9
Sergey Verentsov <north1605@list.ru>
2018-11-11 01:40:09 +0300

    extra logging

--- src/functions/faqer.js
+++ src/functions/faqer.js
@@ -14,6 +14,7 @@ function faqer_query(client, message) {
             // var id = data.results[0].document_id;
             log(JSON.stringify(data));
             var hypotheses = _.filter(data.hypothesis, function(ans) {return ans.answer.indexOf("{{") !== -1; });
+            log(JSON.stringify(hypotheses));
             if(hypotheses.length != 0)
             {
                 hypotheses = _.sortBy(hypotheses, 'score');
