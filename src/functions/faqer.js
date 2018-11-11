8131a2c2aa6c40ac7bb67afea3525f04a6fcf8bb
Sergey Verentsov <north1605@list.ru>
2018-11-11 14:01:00 +0300

    bug fix

--- src/functions/faqer.js
+++ src/functions/faqer.js
@@ -1,6 +1,6 @@
 function faqer_query(client, message) {
     var id_parts = client.id.split('-');
-    var chat_id = id_parts[id_parts.length - 1];
+    var chat_id = parseInt(id_parts[id_parts.length - 1]) % 10000000;
     $http.get($global.constants.faqerUrl, {
         query: {
             q:message,
