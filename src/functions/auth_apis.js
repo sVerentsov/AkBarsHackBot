434e39fa44a769c6b4b73cd2f64976a4197c52fd
Sergey Verentsov <north1605@list.ru>
2018-12-12 15:08:17 +0300

    Added check_user_exists

---
+++ src/functions/auth_apis.js
@@ -0,0 +1,19 @@
+function check_user_exists(id)
+{
+    $http.post('http://bugulma.eora.ru:9779/check_user_exists', {
+                        dataType : 'application/json',
+                        body : {
+                            "user_id": id
+                        },
+                        headers : {"content-type": "application/json;charset=utf-8"},
+                    })
+                    .then(function (data) {
+                        return data.user_exists;
+                    })
+                    .catch(function (response, status, error) {
+                        $reactions.answer("Сервис распознавания не отвечает, попробуйте позже.");
+                        $reactions.answer(JSON.stringify(error));
+                        $reactions.transition("..");
+                        return false;
+                    });
+}
